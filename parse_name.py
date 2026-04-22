982c51fd247b1c6efcae7c979a67d7e0cb473af6
signoiidx <11086731+signoiidx@users.noreply.github.com>
2026-04-22 21:33:16 +0900

    format code and remove excess whitespace

--- parse_name.py
+++ parse_name.py
@@ -1,26 +1,24 @@
-import csv
 import pykakasi
 
 def parse_name(csv_text):
     csv_result = []
-    
-    for pair_text in csv_text: 
+
+    for pair_text in csv_text:
         kks = pykakasi.kakasi()
         result = [kks.convert(pair_text[0]), kks.convert(pair_text[1])]
-        
+
         pair_result = []
-        
-        for category in result: 
+
+        for category in result:
             sentence_roman = ''
-            
-            for item in category: 
+
+            for item in category:
                 chunk_Roman = item['hepburn']
                 sentence_roman += ''.join(chunk_Roman)
-                
+
             typing_count = len(sentence_roman)
             pair_result.extend(tuple([sentence_roman, typing_count]))
 
         csv_result.append(pair_result)
         print(pair_result)
-        
     return csv_result
